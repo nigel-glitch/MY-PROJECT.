57b4803f6eca0e0ca5ad2e19b279d7ca8f800e42
nigel-glitch <hamamitit@gmail.com>
2025-10-27 09:44:03 +0200

    Add files via upload

---
+++ assignmet1.py
@@ -0,0 +1,10 @@
+# Program to check if a number is odd or even
+
+# Get input from the user
+number = int(input("Enter a number: "))
+
+# Check if the number is even or odd
+if number % 2 == 0:
+    print(f"{number} is even.")
+else:
+    print(f"{number} is odd.")
